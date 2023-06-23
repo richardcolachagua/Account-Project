3e4e416d17f6bf62e6e513beabdd33fcb6be85aa
Richard Colachagua <richardcolachagua@gmail.com>
2023-06-22 22:26:03 -0400

    "first account project commits"

---
+++ src/main/java/structure/Main.java
@@ -0,0 +1,12 @@
+package main.java.structure;
+
+import main.java.structure.Account;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account1 = new Account();
+        LocalDate ld = LocalDate.now();
+    }
+}
